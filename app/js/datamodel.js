eda3b7291a21f852e908b4c9ead0b0f1e71c73da
Jorn <Jorn@localhost>
2015-09-18 15:44:10 +0200

    Added amount field, and inline update

--- app/js/datamodel.js
+++ app/js/datamodel.js
@@ -14,14 +14,7 @@ var Datamodel = function Datamodel($http) {
         return $http.get('content/inventory.json');
     };
 
-    this.getPrice = function(name, setCode) {
-        return $http.get('https://api.deckbrew.com/mtg/cards?name=' + encodeURIComponent(name)).then(function(result) {
-            for (var i = 0; i < result.data[0].editions.length; i++) {
-                if (result.data[0].editions[i].set_id === setCode) {
-                    return result.data[0].editions[i].price;
-                }
-            }
-            return null;
-        });
+    this.updateAmount = function(name, amount) {
+        return $http.put('content/inventory.json', {name: name, amount: amount});
     };
 };
