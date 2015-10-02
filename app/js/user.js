5e22a19275188f5bfa87532a82d4b5fd1bcc0a96
Jorn86 <jorn86@gmail.com>
2015-10-02 16:57:48 +0200

    Added google login

---
+++ app/js/user.js
@@ -0,0 +1,22 @@
+angular.module('card-app')
+    .controller('UserController', function ($scope, $http) {
+        $scope.$on('event:google-plus-signin-success', function (event, authResult) {
+            $http.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + authResult.access_token).then(function(result) {
+                $scope.logout = authResult.logout;
+                $scope.userInfo = {
+                    name: result.data.name,
+                    email: result.data.email,
+                    image: result.data.picture
+                };
+
+                $scope.datamodel.authenticateUser({
+                    name: result.data.name,
+                    email: result.data.email,
+                    token: authResult.id_token
+                });
+            });
+        });
+        $scope.$on('event:google-plus-signin-failure', function (event, authResult) {
+            //console.log('sign in failed', event, authResult)
+        });
+    });
