a3f79163a1cdd9cf0ee9abc4262c919587f1bf1d
Jorn86 <jorn86@gmail.com>
2015-10-23 10:54:27 +0200

    Add sort descending

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,6 +5,7 @@ module.exports = function(config) {
         files : [
             'app/bower_components/jquery/dist/jquery.js',
             'app/bower_components/q/q.js',
+            'app/bower_components/underscore/underscore.js',
             'app/bower_components/angular/angular.js',
             'app/bower_components/angular-ui-router/release/angular-ui-router.js',
             'app/bower_components/angular-filter/dist/angular-filter.js',
