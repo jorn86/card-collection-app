cb17f3ea888fcb9626ce6a8f2b79b50ad5b4c745
Jorn86 <jorn86@gmail.com>
2015-10-26 17:45:01 +0100

    Added support for more difficult mana symbols

--- app/js/user.js
+++ app/js/user.js
@@ -1,23 +1,27 @@
 angular.module('card-app')
-    .controller('UserController', function ($scope, $http) {
+    .controller('UserController', function ($scope, $http, $rootScope) {
         $scope.$on('event:google-plus-signin-success', function (event, authResult) {
             $http.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + authResult.access_token).then(function(result) {
-                $scope.logout = authResult.logout;
+                var user = result.data;
                 $scope.userInfo = {
-                    name: result.data.name,
-                    email: result.data.email,
-                    image: result.data.picture
+                    name: user.name,
+                    email: user.email,
+                    image: user.picture
                 };
 
+                console.log(authResult, user)
                 $scope.datamodel.authenticateUser({
-                    name: result.data.name,
-                    email: result.data.email,
-                    token: authResult.id_token,
+                    name: user.name,
+                    email: user.email,
+                    id: user.id,
                     type: 'google'
                 });
+                $rootScope.$broadcast('user', result.data);
             });
         });
+
         $scope.$on('event:google-plus-signin-failure', function (event, authResult) {
             console.log('sign in failed', event, authResult)
+            $rootScope.$broadcast('user', null);
         });
     });
