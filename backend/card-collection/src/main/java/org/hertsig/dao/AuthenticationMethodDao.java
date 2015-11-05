7e253c4e4cf84ca765d3b92533c33db0dd24e377
Jorn Hertsig <jorn86@users.noreply.github.com>
2015-11-05 16:01:58 +0100

    Cleanup

--- backend/card-collection/src/main/java/org/hertsig/dao/AuthenticationMethodDao.java
+++ backend/card-collection/src/main/java/org/hertsig/dao/AuthenticationMethodDao.java
@@ -2,19 +2,21 @@ package org.hertsig.dao;
 
 import java.util.UUID;
 
-import org.hertsig.database.GeneratedKeyMapper;
+import org.hertsig.database.UuidMapper;
 import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.GetGeneratedKeys;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
 
 public interface AuthenticationMethodDao extends AutoCloseable {
     @SqlQuery("SELECT userid AS id FROM authenticationmethod WHERE externalid = :id AND type = :type")
-    @Mapper(GeneratedKeyMapper.class)
+    @Mapper(UuidMapper.class)
     UUID getExistingUser(@Bind("id") String id, @Bind("type") String type);
 
     @SqlUpdate("INSERT INTO authenticationmethod (userid, externalid, type) VALUES (:user, :id, :type)")
-    void insert(@Bind("user") UUID userId, @Bind("id") String id, @Bind("type") String type);
+    @GetGeneratedKeys(UuidMapper.class)
+    UUID create(@Bind("user") UUID userId, @Bind("id") String id, @Bind("type") String type);
 
     void close();
 }
