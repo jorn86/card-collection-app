fe1455b161a574af7dc1c93f40956b554acabd09
Jorn Hertsig <jorn86@users.noreply.github.com>
2015-11-10 14:34:13 +0100

    Auto-redeploy webapp

--- backend/card-collection/src/main/java/org/hertsig/webserver/Webserver.java
+++ backend/card-collection/src/main/java/org/hertsig/webserver/Webserver.java
@@ -1,5 +1,9 @@
 package org.hertsig.webserver;
 
+import java.io.File;
+
+import org.eclipse.jetty.deploy.DeploymentManager;
+import org.eclipse.jetty.deploy.providers.WebAppProvider;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
@@ -7,14 +11,27 @@ import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Webserver {
     public static void main(String... args) throws Exception {
-        ContextHandlerCollection handler = new ContextHandlerCollection();
-        handler.setHandlers(new Handler[]{
-                new WebAppContext("src/main/webapp", "/api"),
-                new WebAppContext("../../webapp/ROOT.war", "/")
+        WebAppProvider webappProvider = new WebAppProvider();
+        webappProvider.setMonitoredDirName("../../webapp/");
+        webappProvider.setScanInterval(1); // how often to scan
+        webappProvider.setExtractWars(true);
+        webappProvider.setTempDir(new File("/opt/jetty/work"));
+
+        ContextHandlerCollection contexts = new ContextHandlerCollection();
+        contexts.setHandlers(new Handler[] {
+                new WebAppContext("src/main/webapp", "/api")
         });
 
+        DeploymentManager deploymentManager = new DeploymentManager();
+        deploymentManager.setContexts(contexts);
+        deploymentManager.setContextAttribute(
+                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+                ".*/servlet-api-[^/]*\\.jar$");
+        deploymentManager.addAppProvider(webappProvider);
+
         Server server = new Server(8080);
-        server.setHandler(handler);
+        server.setHandler(contexts);
+        server.addBean(deploymentManager);
         server.start();
         server.join();
     }
