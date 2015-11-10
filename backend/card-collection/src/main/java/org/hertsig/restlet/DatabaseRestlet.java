7c6732bac311a95587ca49700c29958fab7ed0de
Jorn86 <jorn86@gmail.com>
2015-11-10 14:34:13 +0100

    Auto-redeploy webapp

--- backend/card-collection/src/main/java/org/hertsig/restlet/DatabaseRestlet.java
+++ backend/card-collection/src/main/java/org/hertsig/restlet/DatabaseRestlet.java
@@ -1,7 +1,5 @@
 package org.hertsig.restlet;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,9 +9,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.hertsig.dao.SearchDao;
-import org.hertsig.dto.Card;
 import org.skife.jdbi.v2.DBI;
-
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -35,9 +31,7 @@ public class DatabaseRestlet {
     @Path("search")
     public Object searchCards(@QueryParam("name") String name) {
         try (SearchDao dao = dbi.open(SearchDao.class)) {
-            List<Card> cards = dao.searchCardsByName(name + "%");
-            log.debug("Got {} results for name {}", cards.size(), name);
-            return cards;
+            return dao.searchCardsByName(name + "%");
         }
     }
 }
