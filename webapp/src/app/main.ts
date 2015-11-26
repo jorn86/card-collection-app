6b5bbd7f62573b847eb9c0b90a72c498306bc670
Jorn86 <jorn86@gmail.com>
2015-11-26 17:43:10 +0100

    Started on upgrade to angular 2

---
+++ webapp/src/app/main.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/angular2';
+
+//module cardcollection {
+    @Component({
+        selector: 'menubar',
+        templateUrl: 'app/menubar.html'
+    })
+    export class MenuComponent {
+
+    }
+//}
---
+++ webapp/src/app/versioncheck.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/angular2';
+
+//module cardcollection {
+    @Component({
+        selector: 'versioncheck',
+        templateUrl: 'app/versioncheck.html'
+    })
+    export class VersionCheckComponent {
+
+    }
+//}
