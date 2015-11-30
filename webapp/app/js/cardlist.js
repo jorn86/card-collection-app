5b0039e9b762558c6ab16d68db359c9fe3c6c7fa
Jorn86 <jorn86@gmail.com>
2015-11-30 13:03:37 +0100

    Added some preconstructed decks, and fixed card count with distinct

--- webapp/app/js/cardlist.js
+++ webapp/app/js/cardlist.js
@@ -1,11 +1,11 @@
 angular.module('card-app')
 
     .controller('CardListController', function ($scope) {
-        $scope.mapSetCodeToImage = function(setcode) {
+        $scope.mapSetCodeToImage = function (setcode) {
         };
 
         var types = ['Creature', 'Instant', 'Sorcery', 'Artifact', 'Enchantment', 'Planeswalker', 'Land', 'Other'];
-        var typeGrouping = function(card) {
+        var typeGrouping = function (card) {
             if (card.type) {
                 for (var i = 0; i < types.length; i++) {
                     if (card.type.indexOf(types[i]) >= 0) {
@@ -15,18 +15,26 @@ angular.module('card-app')
             }
             return 'Other';
         };
-        var typeOrder = function(card) {
+        var typeOrder = function (card) {
             return _.indexOf(types, card.$key);
         };
 
-        var colors = {'W': 'White', 'U': 'Blue', 'B': 'Black', 'R': 'Red', 'G': 'Green', 'C': 'Colorless', 'M': 'Multicolored'};
-        var colorGrouping = function(card) {
+        var colors = {
+            'W': 'White',
+            'U': 'Blue',
+            'B': 'Black',
+            'R': 'Red',
+            'G': 'Green',
+            'C': 'Colorless',
+            'M': 'Multicolored'
+        };
+        var colorGrouping = function (card) {
             return colors[color(card.mana)];
         };
-        var colorOrder = function(group) {
+        var colorOrder = function (group) {
             return _.indexOf(_.keys(colors), color(group.$key));
         };
-        var color = function(mana) {
+        var color = function (mana) {
             if (!mana) return 'C';
 
             var color = null;
@@ -49,17 +57,21 @@ angular.module('card-app')
             }, {
                 name: 'Color', group: colorGrouping, order: colorOrder
             }, {
-                name: 'None', group: function() { return 'All' }, order: function() { return 0 }
+                name: 'None', group: function () {
+                    return 'All'
+                }, order: function () {
+                    return 0
+                }
             }],
             groupBy: typeGrouping,
             orderBy: typeOrder,
 
             sortingOptions: [
-                { name: 'Count', field: 'amount' },
-                { name: 'Name', field: 'name' },
-                { name: 'Type', field: 'type' },
-                { name: 'Set', field: 'setcode' },
-                { name: 'Converted Mana Cost', field: 'cmc' }
+                {name: 'Count', field: 'amount'},
+                {name: 'Name', field: 'name'},
+                {name: 'Type', field: 'type'},
+                {name: 'Set', field: 'setcode'},
+                {name: 'Converted Mana Cost', field: 'cmc'}
             ],
             sortDescending: false,
 
@@ -67,28 +79,28 @@ angular.module('card-app')
         };
 
         $scope.grid.currentGrouping = $scope.grid.groupingOptions[0];
-        $scope.onGroupingChange = function() {
+        $scope.onGroupingChange = function () {
             $scope.grid.orderBy = $scope.grid.currentGrouping.order;
             $scope.grid.groupBy = $scope.grid.currentGrouping.group;
         };
 
         $scope.grid.currentSorting = $scope.grid.sortingOptions[4];
 
-        $scope.updateAmount = function(data) {
+        $scope.updateAmount = function (data) {
             $scope.datamodel.updateAmount($scope.deck.id, data.rowId, data.amount);
         };
 
-        $scope.updateGrid = function(cards) {
+        $scope.updateGrid = function (cards) {
             $scope.grid.rows = cards;
         };
 
-        $scope.onSelectAll = function() {
+        $scope.onSelectAll = function () {
             for (var i = 0; i < $scope.grid.rows.length; i++) {
                 $scope.grid.rows[i].selected = $scope.grid.allSelected;
             }
         };
 
-        $scope.getSelected = function() {
+        $scope.getSelected = function () {
             var selected = [];
             for (var i = 0; i < $scope.grid.rows.length; i++) {
                 if ($scope.grid.rows[i].selected) {
@@ -100,16 +112,16 @@ angular.module('card-app')
 
         $scope.cardSearchValue = '';
         var searchResult = null;
-        $scope.searchCallback = function(query) {
-            return $scope.datamodel.searchCardsByName(query.query).then(function(result) {
+        $scope.searchCallback = function (query) {
+            return $scope.datamodel.searchCardsByName(query.query).then(function (result) {
                 return result.data;
             });
         };
-        $scope.searchSelectCallback = function(result) {
+        $scope.searchSelectCallback = function (result) {
             searchResult = result.item;
             return result.item.name;
         };
-        $scope.submitAdd = function() {
+        $scope.submitAdd = function () {
             console.log("add", searchResult)
             if (!searchResult) {
                 return;
@@ -124,14 +136,19 @@ angular.module('card-app')
         };
 
         var currentUserId;
-        $scope.$on('user', function(event, user) {
+        $scope.$on('user', function (event, user) {
             currentUserId = user ? user.id : null;
         });
 
-        $scope.setDeck = function(deck) {
+        $scope.setDeck = function (deck) {
             $scope.deck = deck;
             $scope.editable = deck.userid === currentUserId;
             $scope.updateGrid(deck.cards);
+        };
+
+        $scope.totalAmount = function (group) {
+            console.log(group);
+            return _.map(group, function(value) { return value.amount; }).reduce(function(a,b) { return a+b;}, 0);
         };
     })
 
