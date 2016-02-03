788beaf4ee93f282a89d1dedfef1b20fa6a72966
Jorn Hertsig <jorn86@users.noreply.github.com>
2016-02-03 12:09:44 +0100

    Simplify new buttons

--- webapp/app/js/controllers.js
+++ webapp/app/js/controllers.js
@@ -70,4 +70,33 @@ angular.module('card-app')
                 $scope.reverse = false;
             }
         };
+    })
+
+    .controller('NewDeckController', function($scope, $rootScope, $state) {
+        $scope.tags = [];
+        $scope.input = {
+            tagId: null,
+            name: ''
+        };
+        var addTags = function(node) {
+            $scope.tags.push({id: node.tagId, name: node.tagName});
+            _.forEach(node.children, addTags);
+        };
+        $rootScope.datamodel.getDecks().then(function(result) {
+            _.forEach(result.data.children, addTags);
+        });
+
+        $scope.createTag = function() {
+            $rootScope.datamodel.createTag($scope.input.name, $scope.input.tagId).then(function(result) {
+                $rootScope.$broadcast('reloadUserDecks', {tag: result.data.id});
+            });
+            $scope.closeThisDialog();
+        };
+        $scope.createDeck = function() {
+            $rootScope.datamodel.createDeck($scope.input.name, $scope.input.tagId).then(function(result) {
+                $rootScope.$broadcast('reloadUserDecks', {deck: result.data.id});
+                $state.go('app.deck', {id: result.data.id});
+            });
+            $scope.closeThisDialog();
+        };
     });
