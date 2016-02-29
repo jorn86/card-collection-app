728fbb886f91b8429267cadb851a190d281257de
Server <Server@localhost>
2016-02-29 17:31:39 +0000

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -16,18 +16,18 @@ build() {
 
 deploy_backend() {
     echo "Deploying Backend..."
-    chmod +r backend/card-collection/target/backend.war
     tomcat_deploy "backend/card-collection/target/backend.war" "/backend"
 }
 
 deploy_webapp() {
     echo "Deploying Webapp..."
-    tomcat_deploy "webapp/ROOT.war" "/"
+    tomcat_deploy "webapp/ROOT.war" ""
 }
 
 tomcat_deploy() {
+    echo 'got' $1 $2
     # Upload to tomcat manager and check whether output contains "OK - Deployed application"
-    curl -v -XPUT --data-binary "@$1" \ "http://tomcat:$tomcat_password@localhost:8080/manager/text/deploy?update=true&path=$2" \
+    curl -v -XPUT --data-binary "$1" "http://tomcat:$tomcat_password@localhost:8080/manager/text/deploy?update=true&path=$2" \
         | awk 'BEGIN { e = 1 } /OK - Deployed application/ { e = 0 } { print } END { exit e }'
     echo
 }
