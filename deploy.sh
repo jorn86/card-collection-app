06852b3a3c5cdcd05bbc01b4abe1d7878b30bcd7
Jorn86 <jorn86@gmail.com>
2016-02-29 15:30:24 +0100

    Build & deploy script

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+tomcat_password="$1"
+
+main() {
+    build
+    deploy_backend
+    deploy_webapp
+    echo "Done."
+}
+
+build() {
+    ( cd backend && mvn clean package )
+    ( cd webapp && npm install && npm run-script quick-war )
+}
+
+deploy_backend() {
+    echo "Deploying Backend..."
+    tomcat_deploy "backend/card-collection/target/backend.war" "/backend"
+}
+
+deploy_webapp() {
+    echo "Deploying Webapp..."
+    tomcat_deploy "webapp/ROOT.war" "/"
+}
+
+tomcat_deploy() {
+    # Upload to tomcat manager and check whether output contains "OK - Deployed application"
+    curl -v -XPUT --data-binary "@$1" \ "http://tomcat:$tomcat_password@localhost:8080/manager/text/deploy?update=true&path=$2" \
+        | awk 'BEGIN { e = 1 } /OK - Deployed application/ { e = 0 } { print } END { exit e }'
+    echo
+}
+
+main
