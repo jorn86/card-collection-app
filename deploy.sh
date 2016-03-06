dec6715859d7d3fe77d0cbd55605cd4cce639ba7
Jorn Hertsig <jorn86@users.noreply.github.com>
2016-03-06 09:01:32 +0000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-tomcat_password="$1"
-
 main() {
     build
-    deploy_backend
-    deploy_webapp
+    deploy
     echo "Done."
 }
 
@@ -14,22 +11,9 @@ build() {
     ( cd webapp && npm install && npm run-script quick-war )
 }
 
-deploy_backend() {
-    echo "Deploying Backend..."
-    tomcat_deploy "backend/card-collection/target/backend.war" "/backend"
-}
-
-deploy_webapp() {
-    echo "Deploying Webapp..."
-    tomcat_deploy "webapp/ROOT.war" ""
-}
-
-tomcat_deploy() {
-    echo 'got' $1 $2
-    # Upload to tomcat manager and check whether output contains "OK - Deployed application"
-    curl -v -XPUT --data-binary "$1" "http://tomcat:$tomcat_password@localhost:8080/manager/text/deploy?update=true&path=$2" \
-        | awk 'BEGIN { e = 1 } /OK - Deployed application/ { e = 0 } { print } END { exit e }'
-    echo
+deploy() {
+    sudo cp webapp/ROOT.war /var/lib/tomcat8/webapps
+    sudo cp backend/card-collection/target/backend.war /var/lib/tomcat8/webapps
 }
 
 main
